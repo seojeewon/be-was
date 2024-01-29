837fd87e32c013b839f0ca742e3f5e19253738e2
seojeewon <readyjiwon@gmail.com>
2024-01-29 10:19:04 +0900

    Feat: 헤더만 모아놓은 클래스 생성
    클린코딩을 위함

---
+++ src/main/java/http/HttpHeaders.java
@@ -0,0 +1,11 @@
+package http;
+
+public class HttpHeaders {
+    public static String METHOD = "Method";
+    public static String PATH = "Path";
+    public static String HTTP_VERSION = "Http Version";
+    public static String CONTENT_LENGTH = "Content-Length";
+    public static String STATUS_CODE = "Status-Code";
+    public static String CONTENT_TYPE = "Content-Type";
+    public static String LOCATION = "Location";
+}
