b8ff67b298ce4de7bb345f6033fd735d2e2f20be
seojeewon <readyjiwon@gmail.com>
2024-01-29 13:46:48 +0900

    Chore: Session Duration Time  증가

--- src/main/java/model/Session.java
+++ src/main/java/model/Session.java
@@ -3,28 +3,29 @@ package model;
 import java.time.LocalDateTime;
 
 public class Session {
+    private static int SESSION_DURATION_DAYS = 1;
     private String sessionId;
     private User user;
     private LocalDateTime createDate;
     private LocalDateTime expireDate;
     private LocalDateTime lastAccessDate;
 
-    public Session(String sessionId, User user, LocalDateTime createDate){
-        this.sessionId=sessionId;
-        this.user=user;
-        this.createDate=createDate;
-        this.expireDate=this.createDate.plusSeconds(15);
+    public Session(String sessionId, User user, LocalDateTime createDate) {
+        this.sessionId = sessionId;
+        this.user = user;
+        this.createDate = createDate;
+        this.expireDate = this.createDate.plusDays(SESSION_DURATION_DAYS);
     }
 
-    public String getSessionId(){
+    public String getSessionId() {
         return this.sessionId;
     }
 
-    public LocalDateTime getExpireDate(){
+    public LocalDateTime getExpireDate() {
         return this.expireDate;
     }
 
-    public User getUser(){
+    public User getUser() {
         return this.user;
     }
 
