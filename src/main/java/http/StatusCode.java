743a9f8633522d97321fa5addba3a5653f814074
seojeewon <readyjiwon@gmail.com>
2024-01-29 10:21:43 +0900

    Feat: 응답 상태 코드 클래스
    응답 상태코드 관리

---
+++ src/main/java/http/StatusCode.java
@@ -0,0 +1,8 @@
+package http;
+
+public class StatusCode {
+    public static String OK = "200 OK";
+    public static String FOUND = "302 FOUND";
+    public static String NOT_FOUND = "404 Not Found";
+    public static String INTERNAL_SERVER_ERROR ="500 Internal Server Error";
+}
