ee9b226f0a840b745c7ff2deae58f7373da26921
seojeewon <readyjiwon@gmail.com>
2024-01-29 10:32:56 +0900

    Feat: Get 요청을 처리하는 enum class
    path에 따라 다른 로직 실행

---
+++ src/main/java/webserver/GetType.java
@@ -0,0 +1,83 @@
+package webserver;
+
+import model.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
+import utils.FileLoader;
+import utils.HtmlEditor;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum GetType implements MethodType{
+    AUTHORIZED_HOME("/index.html"){
+        @Override
+        public HttpMessage service(HttpMessage request) throws IOException{
+            //쿠키로 유효한 세션 아이디를 넘겨주는 경우 index.html에 사용자 이름 표시하고 로그인 버튼 없애기
+            Session session = UserService.isValidSession(request);
+            Map<String, String> responseInfo = new HashMap<>();
+            responseInfo.put("HTTP version", request.getHttpVersion());
+            responseInfo.put("Accept", request.getAccept());
+            if(session!=null){
+                byte[] body = HtmlEditor.includeUserNameOnHomePage(session.getUser().getName());
+                responseInfo.put("Content-Length", Integer.toString(body.length));
+                responseInfo.put("Status-Code", "200 OK");
+                return new HttpMessage(responseInfo, body);
+            }
+            //쿠키 없이 요청이 오거나 유효하지 않은 세션 아이디인 경우 변형 없는 index.html 반환
+            else{
+                return OTHERS.service(request);
+            }
+        }
+    },
+    LOGIN_USER_LIST("/user/list.html"){
+        @Override
+        public HttpMessage service(HttpMessage request) throws IOException{
+            Map<String, String> responseInfo = new HashMap<>();
+            responseInfo.put("HTTP version", request.getHttpVersion());
+            responseInfo.put("Accept", request.getAccept());
+            byte[] body = HtmlEditor.listLoginUser();
+            responseInfo.put("Content-Length", Integer.toString(body.length));
+            responseInfo.put("Status-Code", "200 OK");
+            return new HttpMessage(responseInfo, body);
+        }
+    },
+    OTHERS("*"){
+        @Override
+        public HttpMessage service(HttpMessage request) throws IOException {
+            Map<String, String> responseInfo = new HashMap<>();
+            responseInfo.put("HTTP version", request.getHttpVersion());
+            responseInfo.put("Accept", request.getAccept());
+            byte[] body;
+            try{
+                body = FileLoader.loadFileContent(request.getPath(), request.getMimeType());
+                responseInfo.put("Content-Length", Integer.toString(body.length));
+            } catch (IOException e){    //없는 페이지 요청시 404 에러
+                logger.error("Fault Page Request : {}", request.getPath());
+                responseInfo.put("Status-Code", "404 NOT FOUND");
+                body = FileLoader.loadFileContent("/error/not_found.html","html");
+                return new HttpMessage(responseInfo, body);
+            }
+            responseInfo.put("Status-Code", "200 OK");
+            return new HttpMessage(responseInfo, body);
+        }
+    }
+    ;
+
+    private static final Logger logger = LoggerFactory.getLogger(GetType.class);
+    private final String path;
+
+    GetType(String path){
+        this.path=path;
+    }
+
+    public static GetType findByPath(String path){
+        return Arrays.stream(GetType.values())
+                .filter(func -> func.path.equals(path))
+                .findAny()
+                .orElse(OTHERS);
+    }
+}
