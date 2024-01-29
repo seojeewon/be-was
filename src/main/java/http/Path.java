9173e24eb83a4cecb9af8998153aba3b565e8405
seojeewon <readyjiwon@gmail.com>
2024-01-29 11:28:17 +0900

    Feat: url path 상수 클래스 생성

---
+++ src/main/java/http/Path.java
@@ -0,0 +1,15 @@
+package http;
+
+public class Path {
+    public static final String HOME = "/index.html";
+    public static final String JOIN_FORM = "/user/form.html";
+    public static final String JOIN_FORM_FAIL = "/user/form_failed.html";
+    public static final String LOGIN = "/user/login.html";
+    public static final String LOGIN_FAIL = "/user/login_failed.html";
+    public static final String PROFILE = "/user/profile.html";
+    public static final String USER_LIST = "/user/list.html";
+    public static final String QNA_FORM = "/qna/form.html";
+    public static final String QNA_SHOW = "/qna/show.html";
+    public static final String ERROR_500 = "/error/internal_server_error.html";
+    public static final String ERROR_404 = "/error/not_found.html";
+}
