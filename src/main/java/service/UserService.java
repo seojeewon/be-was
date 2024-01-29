4194f59589de72ff3d3700b5f8e40067d4396f4a
seojeewon <readyjiwon@gmail.com>
2024-01-29 10:24:38 +0900

    Feat: 로그인시 유효한 유저 정보인지 확인, 유효 세션인지 확인하는 메서드 추가

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,35 +1,59 @@
 package service;
 
 import db.Database;
-import dto.Login;
-import dto.UserDTO;
+import dto.LoginInfo;
 import model.Session;
 import model.User;
+import webserver.HttpMessage;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class UserService {
-    public String registerUser(UserDTO userInfo){
-        //TODO: 이미 가입한 사람인지 확인
-
+    public static boolean registerUser(User newUser){
+        //이미 존재하는 id일 경우
+        if(isJoinedUser(newUser)){
+            return false;
+        }
         //Database에 새로운 유저 저장
-        User newbie = new User(userInfo.getUserId(), userInfo.getPassword(), userInfo.getName(), userInfo.getEmail());
-        Database.addUser(newbie);
-        return userInfo.getUserId();
+        Database.addUser(newUser);
+        return true;
     }
 
-    public boolean isValidUser(Login loginInfo){
-        User target = Database.findUserById(loginInfo.getUserId());
-        return target != null && target.getPassword().equals(loginInfo.getPassword());
+    public static boolean isValidUser(String body){
+        String[] info = body.split("&");
+        String[] idPart = info[0].split("=");
+        String[] pwPart = info[1].split("=");
+        User target = Database.findUserById(idPart[1]);
+        return target != null && target.getPassword().equals(pwPart[1]);
     }
 
-    public Session login(Login loginInfo){
+    public static Session login(LoginInfo loginInfo){
         //Session Id 만들기
         String sessionId = UUID.randomUUID().toString();
-        Session newSession = new Session(sessionId, loginInfo.getUserId(), LocalDateTime.now());
+        Session newSession = new Session(sessionId, Database.findUserById(loginInfo.getUserId()), LocalDateTime.now());
         Database.addSession(newSession);
         return newSession;
+    }
+
+    public static Session isValidSession(HttpMessage request){
+        //쿠키가 없는 응답인 경우 false 리턴
+        if(request.getCookie()==null){
+            return null;
+        }
+        Session session = Database.findSessionById(request.getCookie());
+        //db에 존재하지 않는 세션이거나 만료된 세션일 경우 false 반환
+        if(session != null && !session.getExpireDate().isBefore(LocalDateTime.now())){
+            return session;
+        }
+        return null;
+    }
+
+    public String findUserName(String userId){
+        return Database.findUserById(userId).getName();
+    }
+
+    private static boolean isJoinedUser(User user){
+        return Database.findUserById(user.getUserId()) != null;
     }
 }
